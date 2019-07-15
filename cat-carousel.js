de436f2f5060b22bfea6bcaa2e0686a78e0b6e6c
vbc221 <vbc221@nyu.edu>
2019-07-15 09:09:38 -0500

    test

--- cat-carousel.js
+++ cat-carousel.js
@@ -1,8 +1,10 @@
-function thumbnailClicks(){}
-$('.thumbnail').on('click', function() {
-  $(this).find('img').attr('src');
-  $(this).find('img').attr('alt');
-});
+function thumbnailClicks() {
+  $('.thumbnail').on('click', function (event) {
+    const imgSrc = $(this).find('img').attr('src')
+    const imgAlt = $(this).find('img').attr('alt')
+
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt)
+  });
 }
 
-$(thumbnailClicks)
+$(thumbnailClicks);
